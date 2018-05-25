766e0566156ba646da1c64c59610899d1ceb0889
Nhan Cao <nhancv92@gmail.com>
2018-05-25 13:03:03 +0700

    update config

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 
-AppRegistry.registerComponent('BeeSightSoftRCT', () => App)
+AppRegistry.registerComponent('BSSDemo', () => App)
